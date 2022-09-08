11083b60b5c10f11ff27f6d6e3e532986c44b7cc
Pedro <pedrosabel08@gmail.com>
2022-09-08 10:21:19 -0300

    eclipse

--- Projeto_Eclipse/src/Cad_Produto.java
+++ Projeto_Eclipse/src/Cad_Produto.java
@@ -4,11 +4,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 
 public class Cad_Produto extends JFrame {
@@ -19,7 +23,9 @@ public class Cad_Produto extends JFrame {
 	private JTextField txtTamanho;
 	private JTextField txtCor;
 	private JTextField txtNome;
-	private JTextField textField;
+	private JTextField txtPreco;
+	private ArrayList<Produto> listaProdutos = new ArrayList<Produto>();
+
 
 	/**
 	 * Launch the application.
@@ -47,52 +53,77 @@ public class Cad_Produto extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblId = new JLabel("ID:");
 		lblId.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblId.setBounds(85, 28, 46, 14);
 		contentPane.add(lblId);
-		
+
 		txtID = new JTextField();
 		txtID.setBounds(192, 27, 118, 20);
 		contentPane.add(txtID);
 		txtID.setColumns(10);
-		
+
 		JLabel lblMarca = new JLabel("Marca:");
 		lblMarca.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblMarca.setBounds(85, 84, 46, 14);
 		contentPane.add(lblMarca);
-		
+
 		txtMarca = new JTextField();
 		txtMarca.setBounds(192, 83, 118, 20);
 		contentPane.add(txtMarca);
 		txtMarca.setColumns(10);
-		
+
 		JLabel lblTamanho = new JLabel("Tamanho:");
 		lblTamanho.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblTamanho.setBounds(85, 109, 71, 14);
 		contentPane.add(lblTamanho);
-		
+
 		txtTamanho = new JTextField();
 		txtTamanho.setBounds(192, 108, 118, 20);
 		contentPane.add(txtTamanho);
 		txtTamanho.setColumns(10);
-		
+
 		JLabel lblCor = new JLabel("Cor:");
 		lblCor.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblCor.setBounds(85, 140, 46, 14);
 		contentPane.add(lblCor);
-		
+
 		txtCor = new JTextField();
 		txtCor.setBounds(192, 139, 118, 20);
 		contentPane.add(txtCor);
 		txtCor.setColumns(10);
-		
+
 		JButton btnNewButton = new JButton("Salvar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String id = txtID.getText();
+				String nome = txtNome.getText();
+				String marca = txtMarca.getText();
+				String tamanho = txtTamanho.getText();
+				String cor = txtCor.getText();
+				String preco = txtPreco.getText();
+
+				Produto c = new Produto();
+
+				c.setID(id);
+				c.setNome(nome);
+				c.setMarca(marca);
+				c.setTamanho(tamanho);
+				c.setCor(cor);
+				c.setPreco(preco);
+
+				listaProdutos.add(c);
+				JOptionPane.showMessageDialog(null, "Cadastro de Produto realizado com sucesso!");
+				atualizarJTable();
+				limparCampos();
+
+			}
+		});
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnNewButton.setBounds(286, 227, 89, 23);
 		contentPane.add(btnNewButton);
-		
+
 		JButton btnLimpar = new JButton("Limpar");
 		btnLimpar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -106,7 +137,7 @@ public class Cad_Produto extends JFrame {
 		btnLimpar.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnLimpar.setBounds(163, 227, 89, 23);
 		contentPane.add(btnLimpar);
-		
+
 		JButton btnFechar = new JButton("Fechar");
 		btnFechar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -116,25 +147,47 @@ public class Cad_Produto extends JFrame {
 		btnFechar.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnFechar.setBounds(42, 229, 89, 23);
 		contentPane.add(btnFechar);
-		
+
 		JLabel lblNome = new JLabel("Nome:");
 		lblNome.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNome.setBounds(85, 59, 46, 14);
 		contentPane.add(lblNome);
-		
+
 		txtNome = new JTextField();
 		txtNome.setBounds(192, 58, 118, 20);
 		contentPane.add(txtNome);
 		txtNome.setColumns(10);
-		
-		JLabel lblPreo = new JLabel("Pre\u00E7o:");
-		lblPreo.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblPreo.setBounds(85, 165, 46, 14);
-		contentPane.add(lblPreo);
-		
-		textField = new JTextField();
-		textField.setBounds(192, 164, 118, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
+
+		JLabel lblPreco = new JLabel("Pre\u00E7o:");
+		lblPreco.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblPreco.setBounds(85, 165, 46, 14);
+		contentPane.add(lblPreco);
+
+		txtPreco = new JTextField();
+		txtPreco.setBounds(192, 164, 118, 20);
+		contentPane.add(txtPreco);
+		txtPreco.setColumns(10);
 	}
-}
+			protected void limparCampos() {
+				txtID.setText("");
+				txtNome.setText("");
+				txtMarca.setText("");
+				txtTamanho.setText("");
+				txtCor.setText("");
+				txtPreco.setText("");
+			}
+			
+			protected void atualizarJTable() {
+				DefaultTableModel modelo = new DefaultTableModel(
+						new Object[][] {
+						},
+						new String[] {
+								"ID, Nome", "Marca", "Tamanho", "Cor", "Preco"
+						}
+						);
+
+			}
+
+	}
+	
+
---
+++ Projeto_Eclipse/src/Produto.java
@@ -0,0 +1,43 @@
+
+public class Produto {
+	private String ID;
+	private String Nome, Marca, Tamanho, Cor;
+	private String preco;
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public String getMarca() {
+		return Marca;
+	}
+	public void setMarca(String marca) {
+		Marca = marca;
+	}
+	public String getTamanho() {
+		return Tamanho;
+	}
+	public void setTamanho(String tamanho) {
+		Tamanho = tamanho;
+	}
+	public String getCor() {
+		return Cor;
+	}
+	public void setCor(String cor) {
+		Cor = cor;
+	}
+	public String getPreco() {
+		return preco;
+	}
+	public void setPreco(String preco) {
+		this.preco = preco;
+	}
+	
+}
