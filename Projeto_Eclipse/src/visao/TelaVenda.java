01cc32e7be3d1f499928e2733ab70d221236cc87
Eduardo <edualessioo@gmail.com>
2022-10-13 11:40:32 -0300

    atualizações tela venda

--- Projeto_Eclipse/src/visao/TelaVenda.java
+++ Projeto_Eclipse/src/visao/TelaVenda.java
@@ -14,13 +14,16 @@ import javax.swing.JTable;
 import javax.swing.JRadioButton;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.JTextField;
+import visao.TelaEstoque;
+import controle.ProdutoBD;
+import modelo.Produto;
 
 public class TelaVenda extends JFrame {
 
 	private JPanel contentPane;
 	private JTable tabelaProdVenda;
-	private JTextField textField;
-	private JTextField textField_1;
+	public JTextField textField;
+	public JTextField txtID;
 
 	/**
 	 * Launch the application.
@@ -43,6 +46,8 @@ public class TelaVenda extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaVenda() {
+		TelaVenda TV = this;
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 790, 519);
 		contentPane = new JPanel();
@@ -52,17 +57,17 @@ public class TelaVenda extends JFrame {
 		
 		JLabel lblFormaDePagamento = new JLabel("Forma de Pagamento:");
 		lblFormaDePagamento.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblFormaDePagamento.setBounds(45, 211, 141, 25);
+		lblFormaDePagamento.setBounds(45, 260, 141, 25);
 		contentPane.add(lblFormaDePagamento);
 		
 		JLabel lblValorPagar = new JLabel("Valor \u00E0 Pagar:");
 		lblValorPagar.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblValorPagar.setBounds(45, 281, 93, 23);
+		lblValorPagar.setBounds(45, 376, 93, 23);
 		contentPane.add(lblValorPagar);
 		
 		JButton btnNewButton = new JButton("Concluir ");
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnNewButton.setBounds(45, 315, 93, 23);
+		btnNewButton.setBounds(45, 410, 93, 23);
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Cancelar");
@@ -74,7 +79,7 @@ public class TelaVenda extends JFrame {
 			}
 		});
 		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnNewButton_1.setBounds(45, 349, 93, 25);
+		btnNewButton_1.setBounds(45, 444, 93, 25);
 		contentPane.add(btnNewButton_1);
 		
 		JButton btnSelecionar = new JButton("Selecionar");
@@ -84,21 +89,21 @@ public class TelaVenda extends JFrame {
 			}
 		});
 		btnSelecionar.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnSelecionar.setBounds(45, 177, 103, 23);
+		btnSelecionar.setBounds(45, 211, 103, 23);
 		contentPane.add(btnSelecionar);
 		
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("\u00C0 vista");
 		rdbtnNewRadioButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		rdbtnNewRadioButton.setBounds(192, 212, 68, 23);
+		rdbtnNewRadioButton.setBounds(45, 294, 68, 23);
 		contentPane.add(rdbtnNewRadioButton);
 		
 		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("A prazo");
 		rdbtnNewRadioButton_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		rdbtnNewRadioButton_1.setBounds(262, 212, 74, 23);
+		rdbtnNewRadioButton_1.setBounds(115, 294, 74, 23);
 		contentPane.add(rdbtnNewRadioButton_1);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(383, 67, 360, 307);
+		scrollPane.setBounds(602, 67, 141, 307);
 		contentPane.add(scrollPane);
 		
 		tabelaProdVenda = new JTable();
@@ -106,14 +111,14 @@ public class TelaVenda extends JFrame {
 			new Object[][] {
 			},
 			new String[] {
-				"ID", "Nome", "Marca", "Tamanho", "Cor", "Preço"
+				"ID", "Quantidade"
 			}
 		));
 		scrollPane.setViewportView(tabelaProdVenda);
 		
 		JLabel lblNewLabel = new JLabel("Carrinho");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel.setBounds(533, 42, 68, 14);
+		lblNewLabel.setBounds(645, 42, 68, 14);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Quantidade:");
@@ -131,15 +136,29 @@ public class TelaVenda extends JFrame {
 		lblNewLabel_2.setBounds(45, 72, 46, 14);
 		contentPane.add(lblNewLabel_2);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(45, 90, 86, 20);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		txtID = new JTextField();
+		txtID.setBounds(45, 90, 46, 20);
+		contentPane.add(txtID);
+		txtID.setColumns(10);
 		
 		JButton btnNewButton_2 = new JButton("Calcular");
 		btnNewButton_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnNewButton_2.setBounds(45, 247, 89, 23);
+		btnNewButton_2.setBounds(45, 342, 89, 23);
 		contentPane.add(btnNewButton_2);
+		
+		JButton btnPesquisar = new JButton("Pesquisar");
+		btnPesquisar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				TabelaProduto TP = new TabelaProduto(TV);
+				TP.setVisible(true);
+				setVisible(false);
+			}
+		});
+		btnPesquisar.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnPesquisar.setBounds(45, 177, 103, 23);
+		contentPane.add(btnPesquisar);
+		
 	}
+	
 }
 
